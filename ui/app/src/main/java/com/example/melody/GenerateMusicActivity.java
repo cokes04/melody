b2bb85eb4d54fbe9c1b065f9a11e4ed53272f416
cokes04 <devokes04@naver.com>
2022-05-29 15:07:24 +0900

    작곡 후 음악재생화면 이동 기능 구현

--- ui/app/src/main/java/com/example/melody/GenerateMusicActivity.java
+++ ui/app/src/main/java/com/example/melody/GenerateMusicActivity.java
@@ -1,7 +1,6 @@
 package com.example.melody;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -11,29 +10,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class GenerateActivity extends AppCompatActivity {
-
-    private ImageView uploaded_image_view;
-    private Button cammera_button;
-    private Button gallery_button;
-
+public class GenerateMusicActivity extends AppCompatActivity {
+    private ImageView uploadedImageView;
+    private Button cammeraButton;
+    private Button galleryButton;
+    private Button generateButton;
     private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generate);
+        setContentView(R.layout.activity_generate_music);
 
-        uploaded_image_view = findViewById(R.id.uploaded_image_view);
+        uploadedImageView = findViewById(R.id.uploaded_image_view);
 
-        cammera_button = findViewById(R.id.take_pictures_button);
-        cammera_button.setOnClickListener( (View v) -> {
+        cammeraButton = findViewById(R.id.take_pictures_button);
+        cammeraButton.setOnClickListener( (View v) -> {
             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
             try {
@@ -48,12 +47,29 @@ public class GenerateActivity extends AppCompatActivity {
 
         });
 
-        gallery_button = findViewById(R.id.image_upload_button);
-        gallery_button.setOnClickListener( (View v) -> {
+        galleryButton = findViewById(R.id.upload_image_button);
+        galleryButton.setOnClickListener( (View v) -> {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT) ;
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(intent, 1);
+        });
+
+        generateButton = findViewById(R.id.generate_music_button);
+        generateButton.setOnClickListener( (View v) -> {
+            // 여기서 작곡 모델 돌려서
+            // 음악 받고 저장 후
+
+            //if(작곡 성공시)
+            if (true) {
+                Intent intent = new Intent(getApplicationContext(), PlayMusicActivity.class);
+                intent.putExtra(PlayMusicActivity.SET_MUSIC_KEY, R.raw.test/*작곡된 음악 파일 ID*/);
+                startActivity(intent);
+            }
+            //else(작곡 실패시)
+            else {
+                Toast.makeText(GenerateMusicActivity.this, "작곡 실패!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
@@ -61,12 +77,12 @@ public class GenerateActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 0 && resultCode == RESULT_OK) {
-            uploaded_image_view.setImageURI(photoUri);
+            uploadedImageView.setImageURI(photoUri);
         }
 
         if(requestCode == 1 && resultCode == RESULT_OK) {
             Uri uri = data.getData();
-            uploaded_image_view.setImageURI(uri);
+            uploadedImageView.setImageURI(uri);
         }
 
 
