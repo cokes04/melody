ac6fef885d9dcdfe3293a4adc2baf367c8544203
cokes04 <devokes04@naver.com>
2022-06-10 13:28:50 +0900

    인트로 화면 생성

---
+++ ui/app/src/main/java/com/melody/ui/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.melody.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro); //xml , java 소스 연결
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
